b4c375cc0247af4a4a9544cb7f3feee22dea5823
Haebun <ksseroo@gmail.com>
2021-07-06 00:17:31 +0900

    - Unit 3. 텍스트 넣기와 버튼 만들기 - 3-4. 버튼에 동작 넣기

--- HaeBun/HelloAndroid/app/src/main/java/com/example/helloandroid/MainActivity.java
+++ HaeBun/HelloAndroid/app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.helloandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void sendMessage(View view) {
+        TextView tv = (TextView)findViewById(R.id.textView);
+        tv.setText("Hi!"); // 화면에 보이는 텍스트를 "Hi!"로 바꾸기
     }
 }
